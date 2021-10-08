5f279cd5c670581cd5ffb3e3b1cb7573f89690d9
Raja Harsh Vardhan Singh <70860738+rhvsingh@users.noreply.github.com>
2021-10-09 00:55:06 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -150,36 +150,3 @@ submitForm.onclick = function(e) {
         }
     }
 }
-
-
-
-
-/* 
-var colors = ["#8b5642", "#6a696b"];
-
-let i = 0;
-
-function frame() {
-    confetti({
-        particleCount: 2,
-        angle: 60,
-        spread: 55,
-        origin: { x: 0 },
-        colors: colors,
-    });
-    confetti({
-        particleCount: 2,
-        angle: 120,
-        spread: 55,
-        origin: { x: 1 },
-        colors: colors,
-    });
-
-    if (Date.now() < Date.now() + 15000) {
-        if (i < 40) {
-            requestAnimationFrame(frame);
-            i++;
-        }
-
-    }
-} */
